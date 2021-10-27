17edd6b49d7576e8a3143a06c389876ae30380bb
cvasquezl <84115739+cvasquezl@users.noreply.github.com>
2021-10-27 12:38:25 -0300

    get

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.use(cors())
 app.use(express.json())
 
 
-app.get('/busqueda', async (req, res) => {
+app.get('/', async (req, res) => {
     let busquedas = await Busqueda.find()
     res.json( busquedas )
 })
